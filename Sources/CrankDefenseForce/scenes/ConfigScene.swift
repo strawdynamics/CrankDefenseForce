7afab88f1cac2444521142bd4d69ca238ee647ee
Paul Straw <paulstraw@paulstraw.com>
2024-11-28 15:44:37 -0800

    Add VersionSprite to ConfigScene

--- Sources/CrankDefenseForce/scenes/ConfigScene.swift
+++ Sources/CrankDefenseForce/scenes/ConfigScene.swift
@@ -1,13 +1,56 @@
 import PlaydateKit
 
 class ConfigScene: BaseScene {
+	class VersionSprite: Sprite.Sprite {
+		let version: String
+		
+		init(version: String) {
+			self.version = version
+			super.init()
+			center = Point(x: 1, y: 1)
+			setSize(width: 40, height: 10)
+			moveTo(Point(x: 400, y: 240))
+			zIndex = 10
+		}
+		
+		override func draw(bounds _: Rect, drawRect _: Rect) {
+			Graphics.pushContext(nil)
+			
+			Graphics.setFont(CdfFont.Nybble4)
+			Graphics.drawMode = .fillWhite
+			let textWidth = CdfFont.Nybble4.getTextWidth(for: version, tracking: 0)
+			Graphics.drawText(version, at: bounds.origin + Point(
+				x: bounds.width - (Float(textWidth) + 4),
+				y: 2
+			))
+			
+			Graphics.popContext()
+		}
+	}
+	
 	let entityStore = EntityStore()
 	
 	var configMenu: ConfigMenu?
 	
+	let versionSprite: VersionSprite
+	
+	override init() {
+				print("Hello from ConfigScene");
+//				let reader = PdxinfoReader(path: "pdxinfo")
+//				let pdxinfo = try! reader.read();
+//				print("vers \(pdxinfo[Utf8Key("version")] ?? "oh no")")
+		versionSprite = VersionSprite(version: "v TODO")
+	}
+	
 	override func update() {
 		entityStore.update()
 		
+		updateInput()
+		
+		entityStore.lateUpdate()
+	}
+	
+	private func updateInput() {
 		let pushed = System.buttonState.pushed
 		
 		if pushed.contains(.b) {
@@ -22,8 +65,6 @@ class ConfigScene: BaseScene {
 		} else if pushed.contains(.down) {
 			configMenu?.selectNext()
 		}
-		
-		entityStore.lateUpdate()
 	}
 	
 	override func enter() {
@@ -37,13 +78,10 @@ class ConfigScene: BaseScene {
 			backgroundType: .configCity
 		)
 		
+		versionSprite.addToDisplayList()
+		
 		configMenu = ConfigMenu(ConfigMenu.Config(
 			entityStore: entityStore,
 		))
-		
-//		print("Hello from ConfigScene");
-//		let reader = PdxinfoReader(path: "pdxinfo")
-//		let pdxinfo = try! reader.read();
-//		print("vers \(pdxinfo[Utf8Key("version")] ?? "oh no")")
 	}
 }
