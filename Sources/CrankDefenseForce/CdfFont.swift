1d5deb518ba1cc80a39135a4b82304b09c10013c
Paul Straw <paulstraw@paulstraw.com>
2024-11-27 18:55:21 -0800

    Add Nico fonts

--- Sources/CrankDefenseForce/CdfFont.swift
+++ Sources/CrankDefenseForce/CdfFont.swift
@@ -2,4 +2,12 @@ import PlaydateKit
 
 enum CdfFont {
 	nonisolated(unsafe) public static let Jamma8x8Mono16: Graphics.Font = try! Graphics.Font(path: "fonts/jamma8x8Mono16")
+	
+	nonisolated(unsafe) public static let NicoBold16: Graphics.Font = try! Graphics.Font(path: "fonts/nicoBold16")
+	
+	nonisolated(unsafe) public static let NicoClean16: Graphics.Font = try! Graphics.Font(path: "fonts/nicoClean16")
+	
+	nonisolated(unsafe) public static let NicoPaint16: Graphics.Font = try! Graphics.Font(path: "fonts/nicoPaint16")
+	
+	nonisolated(unsafe) public static let NicoPups16: Graphics.Font = try! Graphics.Font(path: "fonts/nicoPups16")
 }
