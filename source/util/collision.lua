c8bfaab016b938189898f4013f25fb22f385948d
Paul Straw <paulstraw@paulstraw.com>
2025-06-11 02:10:57 -0700

    Remove old Lua source dir. Whoops!

--- source/util/collision.lua
+++
@@ -1,6 +0,0 @@
---  Collision groups
--- https://sdk.play.date/2.4.2/Inside%20Playdate.html#_collision_groups
-COLL_CITY = 1
-COLL_CPU_ROCKET = 2
-COLL_PLAYER_ROCKET = 3
-COLL_EXPLOSION = 3
--- source/util/maths.lua
+++
@@ -1,3 +0,0 @@
-function roundToNearest(num, mult)
-  return math.floor(num / mult + 0.5) * mult
-end
